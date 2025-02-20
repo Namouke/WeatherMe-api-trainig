91d7ed9a01a5354468af59f902038b4a7e976d99
Namouke <79513377+Namouke@users.noreply.github.com>
2025-02-20 12:25:44 +0100

    test d'appel api pour recevoir les communes de france avec nom,département,coordonnées gps

---
+++ index.js
@@ -0,0 +1,23 @@
+
+async function fecthCityData(cityName) {
+    const url = `https://geo.api.gouv.fr/communes?nom=${cityName}&fields=nom,centre,departement&limit=1`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.length > 0) {
+            const city = data[0];
+            console.log("Ville :", city.nom)
+            console.log("Code Postal :", city.departement);
+            console.log("Latitude :", city.centre.coordinates[1]);
+            console.log("Longitude :", city.centre.coordinates[0]);
+        } else {
+            console.log("Aucune ville trouvée");
+        }
+    } catch (error) {
+        console.error("Erreur lors de l'appel API :", error);
+    }
+}
+
+fecthCityData("Dijon");
